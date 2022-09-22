0aea0042117350ecc4fbedf25f516823a7676a7f
Tien <tieeeeen1994@gmail.com>
2022-09-22 13:29:35 +0800

    Initial commit

---
+++ LavaDestroysNoItems.cs
@@ -0,0 +1,17 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LavaDestroysNoItems
+{
+	public class LavaDestroysNoItems : Mod
+	{
+        public class AllItems : GlobalItem
+        {
+            public override bool? CanBurnInLava(Item item)
+            {
+                return item.type == ItemID.GuideVoodooDoll;
+            }
+        }
+    }
+}
